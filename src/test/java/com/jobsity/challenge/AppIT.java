c73963456cde0d1abb67f1824d05805537990281
Diego Baez <dbaezrenault@gmail.com>
2020-10-01 15:01:19 -0400

    Add first integration test

--- src/test/java/com/jobsity/challenge/AppIT.java
+++ src/test/java/com/jobsity/challenge/AppIT.java
@@ -1,2 +1,32 @@
-package com.jobsity.challenge;public class AppIT {
+package com.jobsity.challenge;
+
+import com.jobsity.challenge.interfaces.BowlingGame;
+import com.jobsity.challenge.models.BowlingGameScoreBoard;
+import com.jobsity.challenge.models.Frame;
+import com.jobsity.challenge.utils.FileParser;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class AppIT {
+
+    @Test
+    public void noScoreIntegrationTest() throws FileNotFoundException {
+
+        FileParser parser = new FileParser();
+        LinkedHashMap<String, List<Frame>> gameFrames = parser.handleFile("no-score.txt");
+
+        gameFrames.forEach((player, frames)->{
+            BowlingGame game =  new BowlingGameScoreBoard(frames);
+            for (Frame frame : frames) {
+                frame.setScore(game.calculateScore(frame));
+
+                assertEquals(0, frame.getScore());
+            }
+        });
+    }
 }
