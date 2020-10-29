3c608c1b32ba872f528e7b1ba961670ad3f8aa7c
Diego Baez <dbaezrenault@gmail.com>
2020-10-29 11:31:20 -0300

    Remove print functionality

--- src/main/java/com/jobsity/challenge/interfaces/BowlingGameInterface.java
+++ src/main/java/com/jobsity/challenge/interfaces/BowlingGameInterface.java
@@ -10,12 +10,7 @@ package com.jobsity.challenge.interfaces;
 
 import com.jobsity.challenge.models.Frame;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-
 public interface BowlingGameInterface {
 
     int calculateScore (Frame frame);
-
-    void printResultBoard(LinkedHashMap<String, List<Frame>> frames);
 }
